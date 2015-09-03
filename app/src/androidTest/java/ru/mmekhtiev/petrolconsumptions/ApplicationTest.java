c723ee0ffd7b58f34f825589546c2219116050f6
Maxim Mekhtiev <maxim.mekhtiev@symphonyteleca.com>
2015-09-03 12:13:13 +0300

    Recreation project with new sdk-version.

--- app/src/androidTest/java/ru/mmekhtiev/petrolconsumptions/ApplicationTest.java
+++ app/src/androidTest/java/ru/mmekhtiev/petrolconsumptions/ApplicationTest.java
@@ -1,4 +1,4 @@
-package ru.mmekhtiev.petrolconsumption.test;
+package ru.mmekhtiev.petrolconsumptions;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/main/java/ru/mmekhtiev/petrolconsumptions/RefuelingListActivity.java
+++ app/src/main/java/ru/mmekhtiev/petrolconsumptions/RefuelingListActivity.java
@@ -1,22 +1,22 @@
-package ru.mmekhtiev.petrolconsumption.ui;
+package ru.mmekhtiev.petrolconsumptions;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class RefuelingListActivity extends Activity {
+public class RefuelingListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(petrolconsumption.mmekhtiev.ru.petrolconsumption.R.layout.activity_refueling_list);
+        setContentView(R.layout.activity_refueling_list);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(petrolconsumption.mmekhtiev.ru.petrolconsumption.R.menu.menu_refueling_list, menu);
+        getMenuInflater().inflate(R.menu.menu_refueling_list, menu);
         return true;
     }
 
@@ -28,7 +28,7 @@ public class RefuelingListActivity extends Activity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == petrolconsumption.mmekhtiev.ru.petrolconsumption.R.id.action_settings) {
+        if (id == R.id.action_settings) {
             return true;
         }
 

7a433e7e638ec873496538c87b5cfc235c74f711
Maxim Mekhtiev <maxim.mekhtiev@symphonyteleca.com>
2016-09-16 12:24:10 +0300

    Test commit from Android Studio

--- app/src/main/java/ru/mmekhtiev/petrolconsumption/MainActivity.java
+++ app/src/main/java/ru/mmekhtiev/petrolconsumption/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final static String[] refuelingNames = { "01.03.2016", "23.03.2016", "1.04.2016", "12.04.2016"};
+    private final static String[] refuelingNames = { "01.03.2014", "23.03.2016", "1.04.2016", "12.04.2016"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
